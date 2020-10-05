db7652bd6127fb9baf63da6d0fb1d4767e1c3040
Junhyeok Yun <junhyeok.dev@gmail.com>
2020-10-05 10:23:46 +0900

    feat: Add edge density analysis

--- ImageAnalyzer.py
+++ ImageAnalyzer.py
@@ -5,29 +5,75 @@ import numpy as np
 
 img = None
 
+WINDOW_SIZE = 7
+STEP = 2
+
 if len(sys.argv) != 2:
     print("Usage: python ImageAnalyzer.py [FILENAME]")
     exit(1)
 else:
     img = cv2.imread(sys.argv[1])
 
+h, w = len(img), len(img[0])
 
 
-def EdgeDensityAnalysis(image):
+def __calcMean(image):
+    pixel_sum = 0
+    size = 0
+    for row in image:
+        for pixel in row:
+            pixel_sum += pixel
+            size += 1
+
+    return pixel_sum / size
+
+
+def totalVariance(image):
+    mean = __calcMean(image)
+    sum_distance = 0
+    size = 0
+    for row in image:
+        for pixel in row:
+            sum_distance += ((pixel - mean) ** 2)
+            size += 1
+
+    return sum_distance / size
+
+
+def edgeDensityAnalysis(image):
     edge = cv2.Canny(image, 50, 100)
-    print(edge)
-    plt.imshow(edge, cmap='gray')
+    plt.imshow(edge,cmap="Greys")
     plt.show()
-    return edge
+
+    total_slc = 0
+    slc_include_edge = 0
+
+    for i in range(w):
+        for j in range(h):
+            if edge[i][j] == 255:
+                slc_include_edge += 1
+            total_slc += 1
+
+    return (slc_include_edge / total_slc) * 100
 
 
-def ColorCompositionAnalysis(image):
+def colorCompositionAnalysis(image):
+    total = [0, 0, 0]
     b, g, r = cv2.split(image)
+    for i in range(w):
+        for j in range(h):
+            total[0] += b[i][j]
+            total[1] += g[i][j]
+            total[2] += r[i][j]
+
+    return total
 
 
-def BackgroundDensityAnalysis(image):
+def backgroundDensityAnalysis(image):
 
     return 0
 
 
-EdgeDensityAnalysis(img)
+print("전체 분산: ", totalVariance(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)))
+print("엣지 밀도: ", edgeDensityAnalysis(img))
+print("색상 구성: ", colorCompositionAnalysis(img))
